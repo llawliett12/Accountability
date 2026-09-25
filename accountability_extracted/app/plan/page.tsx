6d41c3a26bd28f7ddec57aca27ddda8b3c37bc73
llawliett12 <yuvraj99989@gmail.com>
2026-09-25 10:38:29 +0530

    Remove stale pre-refactor files

--- accountability_extracted/app/plan/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function PlanPage(props: {
-  searchParams?: Promise<{ date?: string }>;
-}) {
-  const searchParams = await props.searchParams;
-  redirect(searchParams?.date ? `/?date=${searchParams.date}` : "/");
-}
